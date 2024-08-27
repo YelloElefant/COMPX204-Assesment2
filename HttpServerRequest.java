f753cdf552688581ccd67d19a8daf6b441ee59e9
YelloElefant <alextrotternz@gmail.com>
2024-08-28 11:33:26 +1200

    javadoc comments
    Signed-off-by: YelloElefant <alextrotternz@gmail.com>

--- HttpServerRequest.java
+++ HttpServerRequest.java
@@ -4,16 +4,46 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class HttpServerRequest {
+   /**
+    * the method of the request
+    */
    private String method;
+
+   /**
+    * the file path of the request
+    */
    private String path;
+
+   /**
+    * the http version used by client
+    */
    private String version;
+
+   /**
+    * hashmap of all other headers with key as the name of header and value being
+    * the data in the header
+    */
    private Map<String, String> headers = new HashMap<>();
+
+   /**
+    * counter to count how man headers have been parsed, is used to check if the
+    * request is parsed enough to return
+    */
    private int check = 0;
 
+   /**
+    * this is the main method for the class, it takes the request header line and
+    * pareses the data out of it
+    * 
+    * @param request the request header line
+    */
    public void process(String request) {
+      // checks for a null line or empty line
       if (request == null || request.equals("")) {
          return;
       }
+
+      // gets the mthod, path and version if it is the verb header
       if (request.contains("GET")) {
          String[] lineSplit = request.split(" ");
          this.method = lineSplit[0];
@@ -22,6 +52,8 @@ public class HttpServerRequest {
          return;
       }
 
+      // splits the line at the seperator characters and puts into hashmap as long as
+      // the header has 2 parts key and value
       String[] header = request.split(": ");
       if (header.length == 2) {
          headers.put(header[0], header[1]);
@@ -29,59 +61,60 @@ public class HttpServerRequest {
       }
    }
 
+   /**
+    * finds out whether the request is parsed enough to finish and responsed to the
+    * client
+    * 
+    * @return true if complete, false otherwise
+    */
    public boolean isComplete() {
       return check == 8;
    }
 
+   /**
+    * gets the method of the request
+    * 
+    * @return the method of the request
+    */
    public String getMethod() {
       return method;
    }
 
+   /**
+    * gets the file path requested
+    * 
+    * @return the file path requested
+    */
    public String getPath() {
       return path;
    }
 
+   /**
+    * gets the http version of the client
+    * 
+    * @return the http version
+    */
    public String getVersion() {
       return version;
    }
 
+   /**
+    * gets any other header that doesnt have a custom get method
+    * 
+    * @param headerName header to find in hashmap
+    * @return the header if it exists
+    */
    public String getHeader(String headerName) {
       return headers.get(headerName);
    }
 
+   /**
+    * gets the host of the request
+    * 
+    * @return the host of the request
+    */
    public String getHost() {
       return headers.get("Host").split(":")[0];
    }
 
-   public String getUserAgent() {
-      return headers.get("User-Agent");
-   }
-
-   public String getAccept() {
-      return headers.get("Accept");
-   }
-
-   public String getAcceptLanguage() {
-      return headers.get("Accept-Language");
-   }
-
-   public String getAcceptEncoding() {
-      return headers.get("Accept-Encoding");
-   }
-
-   public String getConnection() {
-      return headers.get("Connection");
-   }
-
-   public String getUpgradeInsecureRequests() {
-      return headers.get("Upgrade-Insecure-Requests");
-   }
-
-   public String getContentType() {
-      return headers.get("Content-Type");
-   }
-
-   public String getContentLength() {
-      return headers.get("Content-Length");
-   }
 }
