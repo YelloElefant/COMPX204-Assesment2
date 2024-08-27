053a9b166d64c5f71918d561bd99c7a3b9cd63c4
YelloElefant <alextrotternz@gmail.com>
2024-08-28 10:51:15 +1200

    checking for null request line
    Signed-off-by: YelloElefant <alextrotternz@gmail.com>

--- HttpServerRequest.java
+++ HttpServerRequest.java
@@ -9,7 +9,7 @@ public class HttpServerRequest {
    private int check = 0;
 
    public void process(String request) {
-      if (request.equals("")) {
+      if (request.equals("") || request == null) {
          return;
       }
       if (request.contains("GET")) {
