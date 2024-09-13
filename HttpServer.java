5bb75dd73bcee477c8b3855efade8da8ace17a11
YelloElefant <alextrotternz@gmail.com>
2024-09-13 19:10:14 +1200

    name fix for env HOST
    Signed-off-by: YelloElefant <alextrotternz@gmail.com>

--- HttpServer.java
+++ HttpServer.java
@@ -57,7 +57,7 @@ public class HttpServer {
 
     private static void initialize() {
         try {
-            String overRideHostIp = System.getenv("HOST_IP");
+            String overRideHostIp = System.getenv("HOST");
             InetAddress host = InetAddress.getLocalHost();
             hostIp = overRideHostIp != null ? host.toString().split("/")[1] : overRideHostIp;
             System.out.println("Host IP: " + hostIp);
